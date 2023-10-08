4dcb03475d36264b96c0d5155cab38a39785af28
Subrahmanyam956 <subrahmanyam956@gmail.com>
2023-10-08 18:44:16 +0530

    Developed the code for the front end

---
+++ WeatherApp/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com', 
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '255e6b4654msh315a34e063bece7p14a041jsn709c99541bea'
+};
